82630577e2a1c08808641cb0ea78fb95dc469f66
amandine arnould <amandine.arnould90@gmail.com>
2021-08-17 14:54:05 +0200

    Buttons functionnal + Added js on navbar background on scroll

--- app/javascript/components/_navbar.js
+++ app/javascript/components/_navbar.js
@@ -1,8 +1,14 @@
 const navbarOpacity = () => {
-  const navbar = document.querySelector(".navbar-lewagon")
-  document.addEventListener('scroll', (event) => {
+  const navbar = document.querySelector(".navbar-lewagon");
 
+  document.addEventListener('scroll', (event) => {
+    const scrollPositionY = window.scrollY;
+    if(scrollPositionY >= 10) {
+      navbar.classList.add("scroll");
+    } else {
+      navbar.classList.remove("scroll");
+    }
   });
 }
 
-export {navbarOpacity}
+export { navbarOpacity }
