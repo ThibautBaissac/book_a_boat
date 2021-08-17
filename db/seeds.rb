b16596bf5b74f666baee92410e80cd333d3e4f90
Andrea Abbou <andrea.abbou@gmail.com>
2021-08-17 10:45:45 +0200

    new users created

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,6 +15,14 @@ User.destroy_all
 puts "Creating users..."
 user_1 = User.new(first_name: "Daniela", last_name: "Zubia", email: "test1@gmail.com", password: "123456")
 user_1.save!
+user_2 = User.new(first_name: "Andrea", last_name: "Abbou", email: "test2@gmail.com", password: "123456")
+user_2.save!
+user_3 = User.new(first_name: "Amandine", last_name: "Arnould", email: "test3@gmail.com", password: "123456")
+user_3.save!
+user_4 = User.new(first_name: "Thibaut", last_name: "Baissac", email: "test4@gmail.com", password: "123456")
+user_4.save!
+user_5 = User.new(first_name: "Arthur", last_name: "Herminie", email: "test5@gmail.com", password: "123456")
+user_5.save!
 
 puts "Creating ships..."
 5.times do
